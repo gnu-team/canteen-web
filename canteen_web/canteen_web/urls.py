2bceab0a16196ce1b521848bf216f0398e5af77a
Austin Adams <git@austinjadams.com>
2016-10-28 21:04:59 -0400

    canteen_web.urls: Serve API at /api, not /
    This way, we can display a normal homepage at /.

--- canteen_web/canteen_web/urls.py
+++ canteen_web/canteen_web/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('canteen.urls')),
+    url(r'^api/', include('canteen.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api_auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
