2bbc113aa8960d300a5ff8f6c5ddf2fe82c8e829
Austin Adams <git@austinjadams.com>
2016-11-12 20:01:20 -0500

    browser: map: Display reports as pins on map
    We use the serialization logic from the canteen_api app to pass in the
    reports to JavaScript, which works out pretty okay. I guess it's a
    little ugly, but all of it is intended to be used together anyway, so
    what the heck.

--- canteen_web/canteen_browser/views.py
+++ canteen_web/canteen_browser/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
+
+from canteen import models
+from canteen_api import serializers
 
 def map(request):
-    return render(request, 'canteen_browser/map.html')
+    reports = models.Report.objects.all()
+    serializer = serializers.ReportSerializer(reports, many=True,
+                                              context={'request': request})
+    reports_json = JSONRenderer().render(serializer.data)
+
+    ctx = {
+        'reports_json': reports_json,
+    }
+
+    return render(request, 'canteen_browser/map.html', ctx)
