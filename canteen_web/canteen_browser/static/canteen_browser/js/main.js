e625802e45139776e9b09ac2eecb00697d7550fb
Austin Adams <git@austinjadams.com>
2016-11-18 21:13:31 -0500

    browser: Move js to separate file
    Move the map-related javascript to a separate file, because it's going
    to grow quite a bit in the next few days.

---
+++ canteen_web/canteen_browser/static/canteen_browser/js/main.js
@@ -0,0 +1,21 @@
+// Main
+var GEORGIA_TECH_LAT = 33.779;
+var GEORGIA_TECH_LONG = -84.398;
+var GEORGIA_TECH_ZOOM = 15;
+
+// Callback invoked by the Google Maps api js once it's loaded
+function initMap() {
+    var map = new google.maps.Map($('#map')[0], {
+        zoom: GEORGIA_TECH_ZOOM,
+        center: { lat: GEORGIA_TECH_LAT, lng: GEORGIA_TECH_LONG }
+    });
+
+    for (var i = 0; i < reports.length; i++) {
+        var report = reports[i];
+        new google.maps.Marker({
+            position: { lat: report.latitude, lng: report.longitude },
+            icon: REPORT_ICON,
+            map: map
+        });
+    }
+}
