d1f86cc5e7f6ed452713df11eb0255e9fe7aa466
Austin Adams <git@austinjadams.com>
2016-11-20 14:46:24 -0500

    browser: Add login page

--- canteen_web/canteen_browser/urls.py
+++ canteen_web/canteen_browser/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from canteen_browser import views
 
 # Set namespace
@@ -6,4 +7,5 @@ app_name = 'canteen_browser'
 
 urlpatterns = [
     url(r'^(?:(?P<active_screen>reports)/)?$', views.map, name='map'),
+    url(r'^login/$', auth_views.login),
 ]
--- canteen_web/canteen_browser/views.py
+++ canteen_web/canteen_browser/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.renderers import JSONRenderer
+from django.contrib.auth.decorators import login_required
 
 from canteen import models
 from canteen_api import serializers
 
 # Return the JavaScript REST API client with the given screen active
+@login_required(login_url='/login/')
 def map(request, active_screen=None):
     reports = models.Report.objects.all()
     serializer = serializers.ReportSerializer(reports, many=True,
