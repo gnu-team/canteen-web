eb70d148b39b8ba862cae0971e57f69430839a92
Austin Adams <git@austinjadams.com>
2016-11-20 12:18:57 -0500

    browser: js: When first showing map, force redraw
    Relevant code comment:
    
        If we initially load a screen other than map (e.g., reports), the
        template on the server side will set #map (the root Google Map
        container) to `display: none'. The Google Maps API seems to interpret
        this as drawing a 0px by 0px map. So when we make the map visible for
        the first time, only a blank gray screen is shown. To fix this, send
        a resize event to the Google Maps API and re-center the map.

--- canteen_web/canteen_browser/static/canteen_browser/js/main.js
+++ canteen_web/canteen_browser/static/canteen_browser/js/main.js
@@ -1,13 +1,14 @@
 // Main
-var GEORGIA_TECH_LAT = 33.779;
-var GEORGIA_TECH_LONG = -84.398;
+var GEORGIA_TECH_LOC = { lat: 33.779, lng: -84.398 };
 var GEORGIA_TECH_ZOOM = 15;
+var drawnMap = active == 'map';
+var map = null;
 
 // Callback invoked by the Google Maps api js once it's loaded
 function initMap() {
-    var map = new google.maps.Map($('#map')[0], {
+    map = new google.maps.Map($('#map')[0], {
         zoom: GEORGIA_TECH_ZOOM,
-        center: { lat: GEORGIA_TECH_LAT, lng: GEORGIA_TECH_LONG }
+        center: GEORGIA_TECH_LOC
     });
 
     for (var i = 0; i < reports.length; i++) {
@@ -17,6 +18,20 @@ function initMap() {
             icon: REPORT_ICON,
             map: map
         });
+    }
+}
+
+// If we initially load a screen other than map (e.g., reports), the
+// template on the server side will set #map (the root Google Map
+// container) to `display: none'. The Google Maps API seems to interpret
+// this as drawing a 0px by 0px map. So when we make the map visible for
+// the first time, only a blank gray screen is shown. To fix this, send
+// a resize event to the Google Maps API and re-center the map.
+function forceMapRedraw() {
+    if (map != null) {
+        drawnMap = true;
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(GEORGIA_TECH_LOC);
     }
 }
 
@@ -38,6 +53,13 @@ function navigateTo(screen) {
         newPath += screen + '/';
     }
     history.pushState({ screen: screen }, 'Screen ' + screen, newPath);
+
+    // Hack to make the Google Map show up properly when it wasn't the
+    // first screen loaded (see comment above forceMapRedraw() for more
+    // info)
+    if (screen == 'map' && !drawnMap) {
+        forceMapRedraw();
+    }
 
     active = screen;
 }
