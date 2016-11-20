678478fec7a152ec4d85ecc41ff4429c5e5bd69d
Austin Adams <git@austinjadams.com>
2016-11-20 17:12:16 -0500

    browser: On registration, assign profile & group

--- canteen_web/canteen_browser/forms.py
+++ canteen_web/canteen_browser/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from canteen.models import Profile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
@@ -7,3 +8,15 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+    def save(self):
+        user = super().save()
+
+        # Make a profile for this user
+        profile = Profile.objects.create(user=user)
+        # For now, make everyone who registers a User
+        # XXX Don't hardcode this
+        profile.set_group('Users')
+        profile.save()
+
+        return user
